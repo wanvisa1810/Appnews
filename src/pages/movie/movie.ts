640dedec9e7d051ff3f41d53dc8f6067a0682266
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-03-23 11:36:42 +0700

    update page science movie

--- src/pages/movie/movie.ts
+++ src/pages/movie/movie.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'; 
 /**
  * Generated class for the MoviePage page.
  *
@@ -14,8 +16,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'movie.html',
 })
 export class MoviePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  movienews: any=0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get('https://www.reddit.com/r/movies/new.json?limit=20')
+    .map(res => res.json())
+    .subscribe(data => {
+      this.movienews = data.data.children;
+    });
   }
 
   ionViewDidLoad() {
--- src/pages/science/science.ts
+++ src/pages/science/science.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'; 
+
 /**
  * Generated class for the SciencePage page.
  *
@@ -14,8 +17,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'science.html',
 })
 export class SciencePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  sciencenews: any=0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get('https://www.reddit.com/r/science/new.json?limit=20')
+    .map(res => res.json())
+    .subscribe(data => {
+      this.sciencenews = data.data.children;
+    });
   }
 
   ionViewDidLoad() {
